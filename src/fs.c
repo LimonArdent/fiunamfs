ce39fe9a1de87f59cbb65ecedc3b75a5b2ff8f8a
LimonArdent <atzin.ugalde.enp5.050@gmail.com>
2024-05-12 21:05:59 -0600

    Quitando stat, corr unsigned int

--- src/fs.c
+++ src/fs.c
@@ -2,11 +2,17 @@
 
 FILE* fiunamfs_open_fs(const char* ruta){
     // Buffer para lecturas
-    char* buf[16];
+    char buf[16];
     memset(buf, 0, 16);
 
-    FILE* img = fopen(ruta, "rb+")
+    FILE* img = fopen(ruta, "rb+");
     if (img == NULL) return NULL;
+
+    // Obtener tamaño de archivo
+    fseek(img, 0L, SEEK_END);
+    uint32_t tam_unidad_real = ftell(img);
+    printf("Tamaño real: %u\n", tam_unidad_real);
+    fseek(img, 0L, SEEK_SET);
 
     // Probar número mágico
     fread(buf, 9, 1, img);
@@ -18,30 +24,30 @@ FILE* fiunamfs_open_fs(const char* ruta){
     // Comprobar versión del sistema de archivos
     fseek(img, 1, SEEK_CUR);
     fread(buf, 5, 1, img);
-    if (strmcmp(buf, "24-2", 4) != 0){
+    if (strncmp(buf, "24-2", 4) != 0){
         errno = ERR_NOFSVER;
         return NULL;
     }
 
     // Obtener tamaño de cluster en bytes
     fseek(img, 25, SEEK_CUR);
-    fread(buf, 4, 1, img);
-    uint32_t tam_cluster = buf[0] + (buf[1]<<8) + (buf[2]<<16) + (buf[3]<<24);
+    fread(buf, 1, 4, img);
+    uint32_t tam_cluster = ((unsigned char) buf[0]) + (((unsigned char) buf[1])<<8) + (((unsigned char) buf[2])<<16) + (((unsigned char) buf[3])<<24);
+    printf("Tamaño de cluster: %u\n", tam_cluster);
 
     // Obtener tamaño de directorio informado
     fseek(img, 1, SEEK_CUR);
-    fread(buf, 4, 1, img);
-    uint32_t tam_dir = buf[0] + (buf[1]<<8) + (buf[2]<<16) + (buf[3]<<24);
+    fread(buf, 1, 4, img);
+    uint32_t tam_dir = ((unsigned char) buf[0]) + (((unsigned char) buf[1])<<8) + (((unsigned char) buf[2])<<16) + (((unsigned char) buf[3])<<24);
+    printf("Tamaño de directorio en clusters: %u\n", tam_dir);
 
     // Obtener tamaño de unidad informado
     fseek(img, 1, SEEK_CUR);
-    fread(buf, 4, 1, img);
-    uint32_t tam_unidad = buf[0] + (buf[1]<<8) + (buf[2]<<16) + (buf[3]<<24);
+    fread(buf, 1, 4, img);
+    uint32_t tam_unidad = ((unsigned char) buf[0]) + (((unsigned char) buf[1])<<8) + (((unsigned char) buf[2])<<16) + (((unsigned char) buf[3])<<24);
+    printf("Tamaño de unidad: %u\n", tam_unidad);
 
     // Comprobar tamaño de unidad
-    struct stat stat_img;
-    fstat(fileno(img), &stat_img);
-    off_t tam_unidad_real = buf.st_size;
     if (tam_unidad_real != tam_unidad*tam_cluster){
         errno = ERR_BADDRVSZ;
         return NULL;
@@ -63,4 +69,6 @@ FILE* fiunamfs_open_fs(const char* ruta){
             return NULL;
         }
     }
+
+    return NULL;
 }
--- src/fs.h
+++ src/fs.h
@@ -10,8 +10,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/stat.h>
-#include <sys/types.h>
 #include <time.h>
 
 #include "error.h"
