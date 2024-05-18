9b3cff72eb104c67aca3ba927a0764e46ea51af9
LimonArdent <atzin.ugalde.enp5.050@gmail.com>
2024-05-18 04:59:35 -0600

    Mejorar representación de fechas y tamaños

--- src/ls.c
+++ src/ls.c
@@ -18,19 +18,24 @@ int fiunamfs_ls(FILE* imagen){
 
             // Leer tamaño de archivo en bytes
             fread(buf, 1, 4, imagen);
-            printf("%-12u", fiunamfs_int32(buf));
+            uint32_t tam_crudo = fiunamfs_int32(buf);
+            printf("%4u%c    ",
+                (tam_crudo>1048575?(tam_crudo/1048576):(tam_crudo>1023?(tam_crudo/1024):tam_crudo)),
+                (tam_crudo>1048575?'M':(tam_crudo>1023?'K':'B'))
+            );
 
             // Leer cluster de archivo
             fread(buf, 1, 4, imagen);
             printf("%-6u", fiunamfs_int32(buf));
 
+            uint32_t fecha[6];
             // Leer fecha de creación de archivo
-            fread(buf, 1, 14, imagen);
-            printf("%-16s", buf);
+            fscanf(imagen, "%4u%2u%2u%2u%2u%2u", fecha, fecha+1, fecha+2, fecha+3, fecha+4, fecha+5);
+            printf("%04u-%02u-%02u %02u:%02u:%02u    ", fecha[0], fecha[1], fecha[2], fecha[3], fecha[4], fecha[5]);
 
             // Leer fecha de última modificación de archivo
-            fread(buf, 1, 14, imagen);
-            printf("%-16s\n", buf);
+            fscanf(imagen, "%4u%2u%2u%2u%2u%2u", fecha, fecha+1, fecha+2, fecha+3, fecha+4, fecha+5);
+            printf("%04u-%02u-%02u %02u:%02u:%02u\n", fecha[0], fecha[1], fecha[2], fecha[3], fecha[4], fecha[5]);
 
             fseek(imagen, 12, SEEK_CUR);
         } else if (arc_tipo == 0x2f){
